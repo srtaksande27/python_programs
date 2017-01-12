7e1151d1cc5472f5417f226dbfa54cfa2e6773d0
srtaksande27 <srtaksande@india.com>
2017-01-12 11:06:43 +0530

    To convert degree centigrade to fahrenheit

---
+++ degree_centigrade_to_fahrenheit.py
@@ -0,0 +1,13 @@
+# -*- coding: cp1252 -*-
+#Assignment3
+
+#WAP to convert temperature from degree centigrade to Fahrenheit. Ex: (°C × 9/5) + 32 = °F
+
+temp = input("Enter the value of temperature in DEGREE CENTIGRADE: ")
+
+fahren= (temp*9/5)+32
+
+print "\n\n The value of temperatare in FAHRENHEIT is {}".format(fahren)
+
+
+raw_input()
