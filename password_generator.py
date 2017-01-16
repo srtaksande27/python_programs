74407d0871695d4c100e7d5a1334a40cdb8bced0
srtaksande27 <srtaksande@india.com>
2017-01-16 20:42:37 +0530

    To Generate Random Password

---
+++ password_generator.py
@@ -0,0 +1,16 @@
+# Random password generator
+
+import string
+from random import *
+
+letters = string.ascii_letters 
+digits = string.digits 
+symbols = string.punctuation
+chars = letters + digits + symbols
+
+min_length = 8
+max_length = 16
+password = "".join(choice(chars) for x in range(randint(min_length, max_length)))
+print  "Your generated password is {}" .format(password)
+
+raw_input()
