ad50fef7cb70c9c9d47e8644fb233c7544b9bfc9
Sandeep R Taksande <srtaksande@india.com>
2017-01-30 12:12:05 +0530

    To get sum of all natural numbers between 1 and input given

---
+++ sum_natural_number.py
@@ -0,0 +1,9 @@
+#To find sum of all natural numbers
+
+number = int(input ("Enter a Number to find sum of natural number:"))
+
+sum_nat = (number*(number+1))/2
+
+print "The Sum is :{} ".format(sum_nat)
+
+print "Press any key to exit"; raw_input()
